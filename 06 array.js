e8cb0a64761b9f0018e3166063fb54d89e9d4308
saman <salmankhanhissam345@gmail.com>
2022-11-23 12:31:35 +0500

    this array

---
+++ 06 array.js
@@ -0,0 +1,38 @@
+
+// refrance data type or non premetive
+
+
+
+// order collection 
+
+
+let array=["apple", "banana","kila"]
+
+console.log(array);
+
+array[1]="khan";
+
+console.log(array);
+
+// all start method ends
+
+// push in array method push data end of the array
+
+array.push("alal")
+
+console.log(array);
+// end se ye vlaue nikal day ga
+let popp =array.pop();
+console.log(popp);
+
+// all ends method ends
+
+
+// statrt method start
+
+
+array.unshift("this add one")
+
+console.log(array);
+
+
