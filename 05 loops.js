6bac1f2dcbd4b79c36e1945925a63edfd9e47047
saman <salmankhanhissam345@gmail.com>
2022-11-23 10:04:51 +0500

    this is Loops file

---
+++ 05 loops.js
@@ -0,0 +1,52 @@
+for (let i=1;i<10;i++){
+
+
+    console.log(i);
+}
+
+
+// for with break
+
+for (i=90; i<=100; i++){
+
+if (i==95) {
+    break;
+}
+console.log(i);
+}
+
+
+// for continue
+
+
+for(i=20;i<=30;i++
+){
+
+if (i==25) {
+
+    continue
+    
+}
+    console.log(i);
+}
+
+
+// this while loops
+let a=0;
+let b=10
+while(a<=b){
+a++
+console.log("this is my for",a);
+}
+
+
+// do while ye aik bar chlay ge agr false bhi ho
+
+
+let c=21
+let d =20
+
+do {
+    console.log("this do while",c);
+    c++;
+} while (c<=d);
